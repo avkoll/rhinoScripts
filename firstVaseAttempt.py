9c418f906469fe35ea901574ff4cc07c60e3bed2
gnawtough <139082778+gnawtough@users.noreply.github.com>
2024-03-24 23:23:12 -0500

    Update firstVaseAttempt.py
    kinda fucky but still works ok

--- firstVaseAttempt.py
+++ firstVaseAttempt.py
@@ -3,17 +3,20 @@ import random
 
 origin = (0, 0, 0)
 edge = (200, 200, 200)
-baseRadius = 8
+baseRadius = 12
 height = 100
 numberOfFins = 40
 widthScale = baseRadius * 3 # value is distance that fins stick out from base
-twist = 180 #degree of twist in degrees
+
 curvesToLoft = [] #array that will hold the curves that will be loft to create the walls of the vase
+midCurveList = [] #ay ay ay if i delete this in the createWalls function it breaks...
 
 isLumpy = True #true adds curves between top and bottom at intervals == numberOfLumps
 numberOfLumps = 6 #number of extra rings between top and bottom to create wavy effect
-lumpScale = 0.1 #not 0
+lumpScale = 1 #not 0
+twist = 120 #degree of twist in degrees
 
+switchBack = False #do you want it to switch directions in the rotation?
 
 
 def createBase():
@@ -39,18 +42,30 @@ def createBase():
     curvesToLoft.append(wavyBase) #add curves between top and bottom below this one and before the top is added i.e. if there ends up being a twist
     return wavyBase
 
-def createWalls(lumpy = False):
+def createWalls(lumpy = False, backForth = False):
     if lumpy == True:
-        midCurveList = []
         midCurveList.append(curvesToLoft[0])
         
         for i in range(0, numberOfLumps):
+            localReversedAmount = 1
+            if backForth:
+                #this applies a random direction to the rotate function. 
+                while True:
+                    randomValue = random.randint(-1, 4) # change positive and negative values here to weight in either direction
+                    if randomValue != 0:
+                        localReversedAmount = randomValue / abs(randomValue)
+                        break
+
+            #raise curve below it
             midCurve = rs.CopyObject(midCurveList[i], [0, 0, (height / numberOfLumps)])
-            midCurveOffset = rs.OffsetCurve(midCurve, edge, ((random.random() + 1) * lumpScale))
-            rs.RotateObject(midCurve, origin, twist / numberOfLumps)
+            #widen curve then rotate it
+            midCurveOffset = rs.OffsetCurve(midCurve, edge, (random.randint(1,10) * lumpScale))
+            midCurveOffset = rs.RotateObject(midCurve, origin, (twist / numberOfLumps) * localReversedAmount)
+            #add the two curves to a list
             midCurveList.append(midCurve)
             curvesToLoft.append(midCurveOffset)
-        
+
+
         #for i in range(1, len(midCurveList)):
             #curvesToLoft.append(midCurveList[i])
             #rs.DeleteObject(midCurve)
@@ -59,14 +74,23 @@ def createWalls(lumpy = False):
         wavyTop = rs.CopyObject(curvesToLoft[0], [0, 0, height])
         rs.RotateObject(wavyTop, origin, twist)
         curvesToLoft.append(wavyTop)
-
     return
 
 def loftAndCap():
-    shell = rs.AddLoftSrf(curvesToLoft)
+    shell = rs.AddLoftSrf(midCurveList)
     rs.CapPlanarHoles(shell)
 
+def deleteAllCurves():
+    # find all curves in doc that are not in locked layer
+    curves = rs.ObjectsByType(4) #4 is type for curves
+    
+    #check if any found
+    if curves:
+        rs.DeleteObjects(curves)
+    else:
+        print("No curves found")
+
 bottom = createBase()
-createWalls(isLumpy)
-print(curvesToLoft)
+createWalls(isLumpy, switchBack)
 loftAndCap()
+#deleteAllCurves()
